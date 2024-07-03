9250c4e33ac755bda9d1612985f5e84549938f67
Ahmed Shawun <faysalahmadshwun@gmail.com>
2024-07-03 13:31:00 +0600

    Give @ once time

--- Check Validate E-Mail Address/Check_Validate_E-Mail_Address.py
+++ Check Validate E-Mail Address/Check_Validate_E-Mail_Address.py
@@ -1,7 +1,10 @@
 userEmail = input(" Enter Your Email : ") #g@g.in , faysalahmadshawun@gmail.com
 if len(userEmail)>=6:
     if userEmail[0].isalpha(): #first letter only for alphabet
-        pass
+        if ("@"in userEmail) and (userEmail.count()==1):
+            pass
+        else:
+            print(" Give @ once time ")
     else:
         print(" Invalid Email ")
 else:
