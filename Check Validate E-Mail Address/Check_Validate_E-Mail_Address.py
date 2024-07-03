d5435630ca483e47e9fbd6eb2c5a2504df1453a6
Ahmed Shawun <faysalahmadshwun@gmail.com>
2024-07-03 13:35:14 +0600

    Must use .

--- Check Validate E-Mail Address/Check_Validate_E-Mail_Address.py
+++ Check Validate E-Mail Address/Check_Validate_E-Mail_Address.py
@@ -2,7 +2,10 @@ userEmail = input(" Enter Your Email : ") #g@g.in , faysalahmadshawun@gmail.com
 if len(userEmail)>=6:
     if userEmail[0].isalpha(): #first letter only for alphabet
         if ("@"in userEmail) and (userEmail.count()==1):
-            pass
+            if (userEmail[-4]==".") ^ (userEmail[-3]=="."):
+                pass
+            else:
+                print("Must use . ")
         else:
             print(" Give @ once time ")
     else:
