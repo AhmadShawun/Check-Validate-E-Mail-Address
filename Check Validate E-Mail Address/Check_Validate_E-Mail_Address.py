50ef115bc0cf92a0f5c5d2a972feaa9d92b0a3ff
Ahmed Shawun <faysalahmadshwun@gmail.com>
2024-07-03 13:38:26 +0600

    Don't use space

--- Check Validate E-Mail Address/Check_Validate_E-Mail_Address.py
+++ Check Validate E-Mail Address/Check_Validate_E-Mail_Address.py
@@ -1,9 +1,14 @@
 userEmail = input(" Enter Your Email : ") #g@g.in , faysalahmadshawun@gmail.com
+k = 0
 if len(userEmail)>=6:
     if userEmail[0].isalpha(): #first letter only for alphabet
         if ("@"in userEmail) and (userEmail.count()==1):
             if (userEmail[-4]==".") ^ (userEmail[-3]=="."):
-                pass
+                for i in userEmail:
+                    if i==i.isspace():
+                        k = 1
+                if k==1:
+                    print("Don't use space")
             else:
                 print("Must use . ")
         else:
